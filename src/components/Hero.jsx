730471065eb3938fbaee510f076409bf2b478669
Swaraj Sampathirao <swarajsampathirao8330@gmail.com>
2024-08-11 23:10:29 +0530

    hero change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,8 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Unlock the potential of community-driven &nbsp;event discovery with{" "}
-            {` `}
+            Unlock the potential of community-driven &nbsp;event discovery with
+            our {` `}
             <span className="inline-block relative">
               EventMingle{" "}
               <img
